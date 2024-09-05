97a9631eac7b0fb90eb93a4a169c0369934700cd
burakyapici747 <burakyapici747@gmail.com>
2024-09-05 21:00:47 +0300

    spring initialize project

---
+++ src/main/java/com/user_messaging_system/UserMessagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.user_messaging_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMessagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMessagingSystemApplication.class, args);
+	}
+
+}
